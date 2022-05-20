43f291c6ce4b703c906d8d41dacd7237b53e858a
rkjar-work <rkjar@yandex.ru>
2022-05-20 15:52:12 +0500

    fonts added

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def notify(image) -> None:
     toast.setTitle("hi", maxLines=1)
     toast.setImage(str(image))
     toast.show()
-    # Path(image).unlink()
 
 
 def add_text(
@@ -40,8 +39,10 @@ def image_editor(image_path: Path, font_path: Path, text: tuple) -> Path:
     with Image.open(image_path) as image:
         width, height = image.size
         font = ImageFont.truetype(font=str(font_path), size=24)
+        text_double_list = []
+        text_double_list.append(text)
 
-        for val in text:
+        for val in text_double_list:
             new_image = copy(image)
             new_image = add_text(xy=(width // 2 - font.getsize(val[0])[0] // 2, 10),
                                  image=new_image,
@@ -63,13 +64,27 @@ def image_editor(image_path: Path, font_path: Path, text: tuple) -> Path:
 
 
 if __name__ == "__main__":
-    image_path = str(Path(Path.cwd(), 'static', 'img', 'blank.jpg'))
-    font_path = str(Path(Path.cwd(), 'static', 'fonts', 'Roboto-Bold.ttf'))
-
-    schedule.every().day.at('14:58:00').do(notify, image=image_editor(image_path, font_path, [["Не пропусти", "уведомления"]]))
-    schedule.every().day.at('14:58:17').do(notify, image=image_editor(image_path, font_path, [["Доброе утро", "Иван"]]))
-
-
+    status =True
+    TEXT = [
+        ["Доброе утро", "Иван"],
+        ["Не пропусти", "уведомления"],
+        ["Приятного аппетита", "Иван"],
+        ["Иван,", "покурил?"],
+        ["Иван,", "чем занимаешься?"],
+        ["Иван", "отчет сделал?"],
+        ["Иван,", "Время расслабиться)))"],
+        ["До завтра,", "Иван"]
+    ]
+    time = 00
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+        for i in TEXT:
+            print(i)
+            image_path = str(Path(Path.cwd(), 'static', 'img', 'blank.jpg'))
+            font_path = str(Path(Path.cwd(), 'static', 'fonts', 'Roboto-Bold.ttf'))
+
+            schedule.every().day.at(f'15:42:{str(time)}'.do(notify, image=image_editor(image_path, font_path, TEXT[i])))
+            # schedule.every(1).minute.do(notify, image=image_editor(image_path, font_path, TEXT.pop()))
+            time += 5
+
+            schedule.run_pending()
+            time.sleep(1)
