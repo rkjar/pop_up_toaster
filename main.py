c31244b16fbd01b630c01eced1e021e65feea8a9
rkjar-work <rkjar@yandex.ru>
2022-05-20 15:02:18 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,49 +1,75 @@
-from copy import copy
-
 import schedule
 import time
+from copy import copy
 from pathlib import Path
-from tinyWinToast.tinyWinToast import Toast
 from PIL import Image, ImageFont, ImageDraw
-from io import BytesIO
+from tinyWinToast.tinyWinToast import Toast
+from text import TEXT
 
 
-def notify():
+def notify(image) -> None:
     toast = Toast()
     toast.setTitle("hi", maxLines=1)
-    toast.setImage(str(Path(Path.cwd(), 'hi.png')))
+    toast.setImage(str(image))
     toast.show()
+    # Path(image).unlink()
 
 
-def add_text(image: Image, xy: tuple, text: str, spacing=0, fill=(0, 0, 0)) -> Image:
+def add_text(
+        image: Image,
+        xy: tuple,
+        text: str,
+        font: ImageFont,
+        stroke_width: int,
+        stroke_fill: tuple,
+        spacing=0,
+        fill=(0, 0, 0)
+) -> Image:
     draw = ImageDraw.Draw(image, "RGB")
     draw.text(xy=xy,
               text=text,
               spacing=spacing,
-              fill=fill)
+              font=font,
+              fill=fill,
+              stroke_width=stroke_width,
+              stroke_fill=stroke_fill)
     return image
 
 
-def image_editor(blank_image: Path, text: tuple):
-    with Image.open(blank_image) as image:
+def image_editor(image_path: Path, font_path: Path, text: tuple) -> Path:
+    with Image.open(image_path) as image:
         width, height = image.size
-        for key, val in text.items():
+        font = ImageFont.truetype(font=str(font_path), size=24)
+
+        for val in text:
             new_image = copy(image)
-            new_image = add_text(image=new_image,
-                             xy=(width // 2, 30),
-                             text=key,
-                             fill=(255, 255, 255))
-            new_image = add_text(image=new_image,
-                             xy=(width // 2, height - 70),
-                             text=val,
-                             fill=(255, 255, 255))
-            new_image.show()
+            new_image = add_text(xy=(width // 2 - font.getsize(val[0])[0] // 2, 10),
+                                 image=new_image,
+                                 text=val[0],
+                                 font=font,
+                                 fill=(255, 255, 255),
+                                 stroke_width=2,
+                                 stroke_fill=(0, 0, 0))
+            new_image = add_text(xy=(width // 2 - font.getsize(val[1])[0] // 2, height - 30),
+                                 image=new_image,
+                                 text=val[1],
+                                 font=font,
+                                 fill=(255, 255, 255),
+                                 stroke_width=2,
+                                 stroke_fill=(0, 0, 0))
+
+        new_image.save(Path(Path(image_path).parent, 'temp.jpg'))
+        return Path(Path(image_path).parent, 'temp.jpg')
 
 
-# schedule.every(1).minutes.do(notify)
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
-text = {'Darova' : 'Vanya', 'Dobroe utro' : 'Vanya', 'Ivan' : 'KEK'}
-image_path = str(Path(Path.cwd(), 'blank.jpg'))
-image_editor(image_path, text)
+if __name__ == "__main__":
+    image_path = str(Path(Path.cwd(), 'static', 'img', 'blank.jpg'))
+    font_path = str(Path(Path.cwd(), 'static', 'fonts', 'Roboto-Bold.ttf'))
+
+    schedule.every().day.at('14:58:00').do(notify, image=image_editor(image_path, font_path, [["Не пропусти", "уведомления"]]))
+    schedule.every().day.at('14:58:17').do(notify, image=image_editor(image_path, font_path, [["Доброе утро", "Иван"]]))
+
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
